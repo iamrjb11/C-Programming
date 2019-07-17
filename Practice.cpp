308219a09133fdd49e5803753f5e702bd87cad3c
iamrjb11 <iamrjb11@gmail.com>
2019-07-18 00:14:20 +0600

    Updated2

--- Practice.cpp
+++ Practice.cpp
@@ -9,19 +9,14 @@ pre=next; next=pre+next; always print pre value;
 */
 #include<stdio.h>
 #include<string.h>
-void fibonacci_series(int pre,int next,int range){
-    if(range!=0){
-        printf("%d ", pre);
-        fibonacci_series(next,pre+next,range-1); 
-    }
-}
+
 
 int main()
 {
     int range;
     printf("Range : ");
     while(scanf("%d",&range)!=EOF){
-        fibonacci_series(0,1,range);
+        
 
         printf("\nRange : ");
 
